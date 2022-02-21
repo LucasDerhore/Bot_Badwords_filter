10b7c822e99f01ac7a3e0c8a933169e167fb8ab8
lucasd <derhore.lucas@gmail.com>
2022-02-21 16:26:24 +0100

    Card Discord Canvas

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 
 console.log(process.env.TOKENBOT);
 
-const { Client, Intents, MessageActionRow, MessageButton } = require("discord.js");
-const wait = require('util').promisify(setTimeout);
+const {
+  Client,
+  Intents,
+  MessageActionRow,
+  MessageButton,
+  MessageAttachment,
+} = require("discord.js");
+const wait = require("util").promisify(setTimeout);
 const Filter = require("bad-words");
 var filter = (filter = new Filter());
 const config = require("./config.json");
-const InvitationsService = require('./src/services/InvitationsService');
+const InvitationsService = require("./src/services/InvitationsService");
+const Canvas = require("canvas");
+const path = require("path");
 
 const client = new Client({
   intents: [
@@ -22,7 +30,6 @@ client.login(process.env.TOKENBOT);
 const prefix = "!";
 
 // BOT RDY START
-
 
 client.on("ready", async () => {
   console.log("Bot online");
@@ -50,10 +57,67 @@ client.on("messageReactionAdd", async (reaction, user) => {
 
 // BOT VERIFY REACTION STOP
 
+// BOT CARD WELCOME
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  //Generating the actual custom Card
+  /*const image = await Canvas.loadImage({
+    avatar: {
+      image: message.author.displayAvatarURL(
+        { format: "png" },
+        { format: "jpg" },
+        { format: "jpeg" }
+      ),
+      outlineWidth: 5,
+    },
+    blur: true,
+    rounded: true,
+    text: {
+      title: "Hello ✌",
+      subtitle: `Welcome to KamagoTree World ${message.author.tag}`,
+      color: `#00000`,
+    },
+    background:
+      "https://cdn.discordapp.com/attachments/941432216595075102/944263582273658970/OPENSEA_BANNER_1400x400.png",
+    theme: "dark",
+  });*/
+
+  // Create a canvas and access the 2d context
+  const canvas = Canvas.createCanvas(700, 250);
+  const ctx = canvas.getContext("2d");
+
+  // Load the background image and draw it to the canvas
+  const background = await Canvas.loadImage(path.join("./Bannniere.png"));
+  let x = 0;
+  let y = 0;
+  ctx.drawImage(background, x, y);
+
+  // Load the user's profile picture and draw it
+  const pfp = await Canvas.loadImage(
+    message.author.displayAvatarURL({
+      format: "png",
+    })
+  );
+  x = canvas.width / 2 - pfp.width / 2;
+  y = canvas.height / 2 - pfp.height / 2;
+  ctx.drawImage(pfp, x, y);
+
+  message.channel.send({
+    files: [new MessageAttachment(canvas.toBuffer())],
+  });
+});
+
+// BOT CARD WELCOME STOP
+
 // BOT FILTER LINK START
 
 client.on("message", async (message) => {
-  if (message.content.includes("https://") || message.content.includes("http://") || message.content.includes("www.")) {
+  if (
+    message.content.includes("https://") ||
+    message.content.includes("http://") ||
+    message.content.includes("www.")
+  ) {
     console.log(
       "deleted " + message.content + " from " + message.author.username
     );
@@ -71,30 +135,37 @@ client.on("message", async (message) => {
     console.log(message.channel.id);
     client.channels.cache.get(message.channel.id).send("Warning swear !");
   }
-  if(message.channelId == "941750479640227890" || message.author.id === "941776823568699393") {
+  if (
+    message.channelId == "941750479640227890" ||
+    (message.author.id === "941776823568699393" &&
+      message.channelId !== "941734057937620994")
+  ) {
     await wait(10000);
     message.delete(1);
   } else if (message.channelId === "944306944179593256") {
-    const response = await InvitationsService.onCodeUse(message.content, message.author.id);
+    const response = await InvitationsService.onCodeUse(
+      message.content,
+      message.author.id
+    );
     client.channels.cache.get(message.channel.id).send(response);
     await wait(10000);
     message.delete(1);
-
   }
 });
 
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isButton()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isButton()) return;
   const userCode = await InvitationsService.generateCode(interaction.user.id);
   if (interaction.message.channelId === "941750479640227890") {
-    const row = new MessageActionRow()
-      .addComponents(
-        new MessageButton()
-          .setCustomId('primary')
-          .setLabel('Copy code')
-          .setStyle('PRIMARY'),
-      );
-    client.channels.cache.get(interaction.channel.id).send(`You referral code is: ${userCode}`, row);
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId("primary")
+        .setLabel("Copy code")
+        .setStyle("PRIMARY")
+    );
+    client.channels.cache
+      .get(interaction.channel.id)
+      .send(`You referral code is: ${userCode}`, row);
   }
 });
 // BOT FILTER BAD WORDS STOP
